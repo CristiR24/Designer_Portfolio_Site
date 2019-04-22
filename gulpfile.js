e958d6bd5aba457a66898cff3adee3ebaa3d0c6f
Cristi Rusu <www.cristir@gmail.com>
2019-04-22 11:31:23 +0300

    implement pug.js and rename some classes

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,22 @@ const sass = require("gulp-sass");
 const browserSync = require("browser-sync").create();
 const cleanCSS = require("gulp-clean-css");
 const autoprefixer = require("gulp-autoprefixer");
-const cache = require("gulp-cache");
 const del = require("del");
 const babel = require("gulp-babel");
 const terser = require("gulp-terser");
+const pug = require("gulp-pug");
+
+const {src, dest, watch, parallel, series} = require("gulp");
+
+const dir = {
+    html: "src/*.html",
+    pug: "src/pug/**/*.pug",
+    fonts: "src/fonts/**/*.otf",
+    images: "src/images/**/*.+(png|jpg|jpeg|gif|svg)",
+    scss: "src/scss/**/*.scss",
+    css: "src/css",
+    js: "src/js/**/*.js"
+};
 
 function server(done) {
     browserSync.init({
@@ -23,55 +35,63 @@ function browserSyncReload(done) {
 }
 
 function clean() {
-    return del(["app/*", "src/css/*"]);
+    return del(["app/*", "src/css/*.css"]);
 }
 
 function html() {
-    return gulp.src("src/*.html").pipe(gulp.dest("app"));
+    return src(dir.html)
+        .pipe(dest("app"));
+}
+
+function pugCompile() {
+    return src(dir.pug)
+        .pipe(pug())
+        .pipe(dest("src"));
 }
 
 function fonts() {
-    return gulp.src("src/fonts/**/*.otf").pipe(gulp.dest("app/fonts"));
+    return src(dir.fonts)
+        .pipe(dest("app/fonts"));
 }
 
 function images() {
-    return gulp.src('src/images/**/*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(gulp.dest('app/images'))
+    return src(dir.images)
+        .pipe(dest("app/images"))
 }
 
 function styles() {
-    return gulp
-        .src("src/scss/**/*.scss")
+    return src(dir.scss)
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(dest(dir.css))
         .pipe(autoprefixer(["last 15 versions"]))
         .pipe(cleanCSS())
-        .pipe(gulp.dest("app/css"))
+        .pipe(dest("app/css"))
         .pipe(browserSync.stream());
 }
 
 function script() {
-    return gulp
-        .src("src/js/**/*.js")
+    return src(dir.js)
         .pipe(terser())
-        .pipe(gulp.dest("app/js"));
+        .pipe(dest("app/js"));
 }
 
 function watchFiles() {
-    gulp.watch("src/*.html", gulp.parallel(html, browserSyncReload));
-    gulp.watch("src/Fonts/**/*.otf", gulp.parallel(fonts, browserSyncReload));
-    gulp.watch('src/images/**/*.+(png|jpg|jpeg|gif|svg)', gulp.parallel(images, browserSyncReload))
-    gulp.watch("src/scss/**/*.scss", styles);
-    gulp.watch("src/js/**/*.js", gulp.parallel(script, browserSyncReload));
+    watch(dir.pug, pugCompile);
+    watch(dir.html, parallel(html, browserSyncReload));
+    watch(dir.fonts, parallel(fonts, browserSyncReload));
+    watch(dir.images, parallel(images, browserSyncReload));
+    watch(dir.scss, styles);
+    watch(dir.js, parallel(script, browserSyncReload));
 }
 
 // complex tasks
-const build = gulp.series(clean, gulp.parallel(html, fonts, images, styles, script));
-const serve = gulp.series(build, gulp.parallel(watchFiles, server));
+const build = series(clean, parallel(html, pugCompile, fonts, images, styles, script));
+const serve = series(build, parallel(watchFiles, server));
 
 // export tasks
 exports.clean = clean;
 exports.html = html;
+exports.pugCompile = pugCompile;
 exports.fonts = fonts;
 exports.images = images;
 exports.styles = styles;
