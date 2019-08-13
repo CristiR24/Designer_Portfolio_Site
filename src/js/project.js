a314134f2ee774180f55a9f382a8d32a74db108b
Cristi Rusu <cristi.rusu.v@gmail.com>
2019-08-13 13:02:22 +0300

    fix shaky slider

--- src/js/project.js
+++ src/js/project.js
@@ -11,6 +11,7 @@ addOnLoad(() => {
 
 const overlay = document.querySelector('.js-slider__overlay');
 const handle = document.querySelector('.js-slider__handle');
+const separator = document.querySelector('.js-slider__separator');
 const slider = document.querySelector('.js-slider');
 let leftOffset = slider.getBoundingClientRect().left;
 let sliderWidth = slider.getBoundingClientRect().width;
@@ -20,13 +21,22 @@ const getPos = event => event.pageX - leftOffset;
 function slide(pos, transition) {
     if (pos < sliderWidth) {
         const percent = pos / sliderWidth * 100;
-        overlay.style.width = `${percent}%`;
+        overlay.style.clipPath = `
+            polygon(0% 0%, ${percent}% 0%, ${percent}% 100%, 0% 100%)
+        `;
+        separator.style.left = `${percent}%`;
+        handle.style.left = `${percent}%`;
+
         overlay.style.transition = transition;
+        separator.style.transition = transition;
+        handle.style.transition = transition;
     }
 }
 
-handle.addEventListener('mousedown', () => {
+handle.addEventListener('mousedown', (e) => {
+    e.preventDefault();
     function updateSlider(event) {
+        event.preventDefault();
         if (event.buttons === 0) {
             slider.style.cursor = 'pointer';
             slider.removeEventListener('mousemove', updateSlider);
@@ -39,8 +49,10 @@ handle.addEventListener('mousedown', () => {
     slider.addEventListener('mousemove', updateSlider);
 });
 
-handle.addEventListener('touchstart', () => {
+handle.addEventListener('touchstart', (e) => {
+    e.preventDefault();
     function updateSlider(event) {
+        event.preventDefault();
         const pos = getPos(event.touches[0]);
         slide(pos, '');
     }
